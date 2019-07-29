5f10cab3ebb3ac7c4b198bb509eea32eec5d1b33
Laury-dev <laury.balard@gmail.com>
2019-07-29 11:10:39 +0200

    mcv qui afiiche les posts

--- model/bdConfig.php
+++ model/bdConfig.php
@@ -1,10 +1,11 @@
 <?php
-require_once 'config.php';
+    require_once 'config.php';
 
 // Fonction de connection a la base de donnée
 function connect(){
+    global $acces, $login, $pwd;
     try {
-        $db = new PDO($acces,$login,$pwd);
+        $db = new PDO($acces, $login, $pwd);
         return $db;
     } catch (Exception $excep) {
         die('Error : '.$excep->getMessage());
--- view/displayTravels.php
+++ view/displayTravels.php
@@ -1,7 +1,7 @@
 <?php
 
 ob_start();
-
+echo '
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,10 +12,10 @@ ob_start();
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body>
+<body>';
 
-<div class="container">
-foreach ($travels as $travel){
+echo '<div class="container">';
+foreach ($travels as $travel) {
     if($travel['image']  != 'null'){
         echo'<div class="row justify-content-md-center mb-3 mt-3"><div class="card" style="width: 75%;"><img src="'.$travel['image'].'" class="card-img-top" alt=""><div class="card-body">';
         echo '<h2 class="card-title">'.$travel['title'].'</h2>';
@@ -26,10 +26,14 @@ foreach ($travels as $travel){
     }
     
 }
-</div>
-
+echo '</div>';
+echo '
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>       
 </body>
-</html>
+</html>';
+
+$content = ob_get_clean();
+
+echo $content;
